79c89dec12af52ac3c48b93dc5c686a929f23e42
Piet van Oostrum <piet@vanoostrum.org>
2014-05-12 16:17:10 +0200

    Added unit tests for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -31,3 +31,39 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+## Unit testing function
+## This is not really part of the assignment requirements,
+## but I wrote it to test that everything is working OK.
+## For everything that doesn't work correctly, a warning is printed
+## If there are no problems, it prints "Everything OK!!"
+
+unittest <- function() {
+
+     # Matrices used for the test
+     mat1 = matrix(c(2, 0, 0, 2), nrow=2)
+     mat1inv = solve(mat1)
+     mat2 = matrix(c(1, 2, 3, 4), nrow=2)
+     mat2inv = solve(mat2)
+     OK = TRUE
+
+     # Check that makeCacheMatrix works
+     mat = makeCacheMatrix(mat1)
+     if (!identical(mat1, mat$get())) {
+          warning("Get doesn't return proper value")
+          OK = FALSE
+     }
+     mat$set(mat2)
+     if (!identical(mat2, mat$get())) {
+          warning("Get doesn't return the set value")
+          OK = FALSE
+     }
+     mat$setinverse(mat1inv)
+     if (!identical(mat1inv, mat$getinverse())) {
+          warning("Getinverse doesn't return the setinverse value")
+          OK = FALSE
+     }
+     if (OK)
+          message("Everything OK!!")
+     
+}
