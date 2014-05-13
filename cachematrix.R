ef32c757208d1943267efbf4f941c3e6e5a5939b
Piet van Oostrum <piet@vanoostrum.org>
2014-05-13 19:13:11 +0200

    Added a general comment describing the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains a pair of functions to cache inverses of square matrices.
+## It contains the following functions:
+##
+##   makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+##   cacheSolve: This function computes the inverse of the special "matrix" returned by
+##     makeCacheMatrix above. If the inverse has already been calculated (and the matrix
+##     has not changed), then cacheSolve retrieves the inverse from the cache.
 
 ## The function makeCacheMatrix creates a special "matrix" object,
 ## which is really a list containing 4 "methods"" to
